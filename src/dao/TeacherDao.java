80665de6fd928e733c034595099addceadd79089
MarySue2116 <ooi2372410@stu.o-hara.ac.jp>
2024-06-06 12:05:46 +0900

    DAOの更新

---
+++ src/dao/TeacherDao.java
@@ -0,0 +1,16 @@
+package dao;
+
+import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.Teacher;
+
+public class TeacherDao extends Dao{
+    public Teacher get(String id) throws Exception{
+    }
+    public Teacher login(String id, String password) throws Exception{
+    }
+}
