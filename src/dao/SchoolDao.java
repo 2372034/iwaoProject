75a077e982c1b73950556a2a1072cf1be6722abc
MarySue2116 <ooi2372410@stu.o-hara.ac.jp>
2024-06-07 11:39:48 +0900

    SchoolDaoの制作

---
+++ src/dao/SchoolDao.java
@@ -0,0 +1,62 @@
+package dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.util.ArrayList;
+
+import bean.School;
+import bean.Student;
+
+public class SchoolDao extends Dao{
+    public School get(String cd) throws Exception{
+    //学生インスタンスを初期化
+    School school = new Student();
+    //データベースへのコネクションを確立
+    Connection connection = getConnection();
+    //プリペアドステートメント
+    PreparedStatement statement =  null;
+    
+    try{
+        //プリペアードステートメントにSQL文をセット
+        statement = connection.prepareStatement("SELECT * FROM school WHERE cd=?");
+        //プリペアードステートメントに学生番号をバインド
+        statement.setString(1,cd);
+        //プリペアードステートメントを実行
+        ResultSet rSet = statement.executeQuery();
+
+        if (rSet.next()){
+            //リザルトセットが存在する場合
+            //学生インスタンスに検索結果をセット
+            school.setCd(rSet.getString("cd"));
+            school.setName(rSet.getString("name"));
+        }else{
+            //リザルトセットが存在しない場合
+            //学生インスタンスにnullをセット
+            school = null;
+        }
+    }catch (Exception e){
+        throw e;
+    }finally{
+        //プリペアードステートメントを閉じる
+        if (school != null) {
+            try{
+                statement.close();
+            }catch (SQLException sqle){
+                throw sqle;
+            }
+        }
+        //コネクションを閉じる
+        if (connection != null) {
+            try{
+                connection.close();
+            }catch (SQLException sqle){
+                throw sqle;
+            }
+        }
+    }
+    return school;
+    }
+}
