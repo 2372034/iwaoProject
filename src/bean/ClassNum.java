ba0ee00bc5dfe8417ba9f639f1e70eaf33da7ccb
KyoArina <ooi2372031@stu.o-hara.ac.jp>
2024-06-10 10:52:18 +0900

    ClassNum.javaを作成

---
+++ src/bean/ClassNum.java
@@ -0,0 +1,24 @@
+package bean;
+
+import java.io.Serializable;
+public class ClassNum implements Serializable {
+
+	private School school;
+	private String num;
+
+
+	public School getSchool() {
+		return school;
+	}
+	public void setSchool(School school) {
+		this.school = school;
+	}
+	public String getNum() {
+		return num;
+    }
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+}
+
