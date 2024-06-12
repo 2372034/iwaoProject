39e49c5c4eac3aeac63d200f777f267c87f95a06
threi309 <ooi2372405@stu.o-hara.ac.jp>
2024-06-12 11:30:56 +0900

    SrudentListActionを修正

--- src/score/StudentListAction.java
+++ src/score/StudentListAction.java
@@ -3,6 +3,8 @@ package score;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpSession;
 import bean.Student;
 import bean.Teacher;
 import dao.ClassNumDao;
+import dao.StudentDao;
 import tool.Action;
 
 public class StudentListAction extends Action {
@@ -42,19 +45,19 @@ public class StudentListAction extends Action {
 
 		if (entYear != 0 && !classNum.equals("0")) {
 		    // 入学年度とクラス番号を指定
-		    students = sDao.filter(teacher.getSchool(), entYear, classNum, isAttend);
+		    Students = sDao.filter(teacher.getSchool(), entYear, classNum, isAttend);
 		} else if (entYear != 0 && classNum.equals("0")) {
 		    // 入学年度のみ指定
-		    students = sDao.filter(teacher.getSchool(), entYear, isAttend);
+		    Students = sDao.filter(teacher.getSchool(), entYear, isAttend);
 		} else if (entYear == 0 && (classNum == null || entYear == 0 && classNum.equals("0"))) {
 		    // 指定なしの場合
 		    // 全学生情報を取得
-		    students = sDao.filter(teacher.getSchool(), isAttend);
+		    Students = sDao.filter(teacher.getSchool(), isAttend);
 		} else {
 		    errors.put("f1", "クラスを指定する場合は入学年度も指定してください");
 		    request.setAttribute("errors", errors);
 		    // 全学生情報を取得
-		    students = sDao.filter(teacher.getSchool(), isAttend);
+		    Students = sDao.filter(teacher.getSchool(), isAttend);
 		}
 
 		if (entYearStr != null) {
@@ -66,7 +69,7 @@ public class StudentListAction extends Action {
 		List<Integer> entYearSet = new ArrayList<>();
 
 		// 10年前から1年後まで年をリストに追加
-		for (int i = year - 10; i < year + 1; i++) {
+		for (int i = Year - 10; i < Year + 1; i++) {
 		    entYearSet.add(i);
 		}
 
@@ -84,7 +87,7 @@ public class StudentListAction extends Action {
 		}
 
 		// リクエストに学生リストをセット
-		request.setAttribute("students", students);
+		request.setAttribute("students", Students);
 
 		// リクエストにデータをセット
 		request.setAttribute("class_num_set", list);
