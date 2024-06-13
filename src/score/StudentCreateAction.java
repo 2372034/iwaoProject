7e2216e24b6209ecfb0b8a0a004645b5cc4dfb96
threi309 <ooi2372405@stu.o-hara.ac.jp>
2024-06-13 10:15:58 +0900

    学生登録のAction作成

--- src/score/StudentCreateAction.java
+++ src/score/StudentCreateAction.java
@@ -10,15 +10,16 @@ import dao.StudentDAO;
 import tool.Action;
 
 public class StudentCreateAction extends Action {
-	public void execute(
-		HttpServletRequest request, HttpServletResponse response
-	) throws Exception {
+    public void execute(
+        HttpServletRequest request, HttpServletResponse response
+    ) throws Exception {
 
-		// コース情報を取得
-		StudentDAO dao=new StudentDAO();
-		List<Student> courseList=dao.studentAll();
-		request.setAttribute("studentList", studentList);
-		// FrontControllerを使用しているためreturn文でフォワードできる
-		return "student_registration.jsp";
-	}
+        // 学生情報を取得
+        StudentDAO dao = new StudentDAO();
+        List<Student> studentList = dao.studentAll();
+        request.setAttribute("studentList", studentList);
+
+        // JSPへのフォワード
+        request.getRequestDispatcher("student_registration.jsp").forward(request, response);
+    }
 }
--- src/score/StudentCreateExecuteAction.java
+++ src/score/StudentCreateExecuteAction.java
@@ -8,32 +8,35 @@ import dao.StudentDAO;
 import tool.Action;
 
 public class StudentCreateExecuteAction extends Action {
-	public void execute(
-		HttpServletRequest request, HttpServletResponse response
-	) throws Exception {
-		// ユーザーからの入力値を受け取る
-		int ent_Year=Integer.parseInt(request.getParameter("entYear"));
-		int no=Integer.parseInt(request.getParameter("no"));
-		String name=request.getParameter("name");
-		String classNum=request.getParameter(request.getParameter("classNum"));
+    public void execute(
+        HttpServletRequest request, HttpServletResponse response
+    ) throws Exception {
+        // ユーザーからの入力値を受け取る
+        int entYear = Integer.parseInt(request.getParameter("entYear"));
+        int no = Integer.parseInt(request.getParameter("no"));
+        String name = request.getParameter("name");
+        String classNum = request.getParameter("classNum");
 
-		// Studentビーンに設定
-		Student student=new Student();
-		student.setEntYear(ent_Year);
-		student.setName(name);
-		student.setClassNum(classNum);
-		// StudentDAOインスタンスを生成
-		StudentDAO dao=new StudentDAO();
-		// StudentDAOのstudentInsertメソッドを実行してデータベースに登録
-		int line = dao.postFilter(student);
+        // Studentビーンに設定
+        Student student = new Student();
+        student.setEntYear(entYear);
+        student.setNo(no);
+        student.setName(name);
+        student.setClassNum(classNum);
 
-		// lineが0でなければ登録成功
-		if (line != 0) {
-			request.setAttribute("message", "登録しました");
-		} else {
-			request.setAttribute("message", "登録に失敗しました");
-		}
-		return "student_completion_of_registration.jsp";
-	}
+        // StudentDAOインスタンスを生成
+        StudentDAO dao = new StudentDAO();
+        // StudentDAOのinsertメソッドを実行してデータベースに登録
+        boolean isSuccess = dao.save(student);
+
+        // 登録成功かどうかでメッセージを設定
+        if (isSuccess) {
+            request.setAttribute("message", "登録しました");
+        } else {
+            request.setAttribute("message", "登録に失敗しました");
+        }
+
+        // フォワード先のJSPに遷移
+        request.getRequestDispatcher("student_completion_of_registration.jsp").forward(request, response);
+    }
 }
-
