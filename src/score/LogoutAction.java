c24ff2192b79a47a670fbd71e73f31f37b49f998
threi309 <ooi2372405@stu.o-hara.ac.jp>
2024-06-14 11:45:17 +0900

    LogoutActionを更新

--- src/score/LogoutAction.java
+++ src/score/LogoutAction.java
@@ -1,5 +1,27 @@
 package score;
 
-public class LogoutAction {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import tool.Action;
+
+public class LogoutAction extends Action {
+
+    @Override
+    public void execute(HttpServletRequest request, HttpServletResponse response) {
+        // セッションを取得して、ログアウト処理を行う
+        HttpSession session = request.getSession();
+        session.invalidate(); // セッションを無効にする（セッションからユーザー情報を削除）
+
+        // ログアウト後の画面にフォワードする
+        String forwardJsp = "/score/logout.jsp"; // ログアウト後に表示するJSPのパスを指定する
+
+        // フォワード先のJSPにリクエストを転送する
+        try {
+            request.getRequestDispatcher(forwardJsp).forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
