bbdc11c4b1f1aa8e5b5431c2752f0fe3efab1a35
ooi2372412 <ooi2372412@stu.o-hara.ac.jp>
2024-06-18 11:21:44 +0900

    学生登録機能の完成（仮）

--- src/score/StudentCreateExecuteAction.java
+++ src/score/StudentCreateExecuteAction.java
@@ -2,8 +2,11 @@ package score;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import bean.School;
 import bean.Student;
+import bean.Teacher;
 import dao.StudentDao;
 import tool.Action;
 
@@ -11,8 +14,14 @@ public class StudentCreateExecuteAction extends Action {
     public void execute(
         HttpServletRequest request, HttpServletResponse response
     ) throws Exception {
+    	HttpSession session=request.getSession();
+        Teacher teacher = (Teacher) session.getAttribute("user");
+
+     // TeacherオブジェクトからSchoolオブジェクトを取得
+        School school = teacher.getSchool();
+
         // ユーザーからの入力値を受け取る
-        int entYear = Integer.parseInt(request.getParameter("ent_year	")); // 入学年度
+        int entYear = Integer.parseInt(request.getParameter("ent_year")); // 入学年度
         String no = request.getParameter("no"); // 学生番号
         String name = request.getParameter("name"); // 氏名
         String classNum = request.getParameter("class_num"); // クラス
@@ -23,6 +32,13 @@ public class StudentCreateExecuteAction extends Action {
         student.setNo(no);
         student.setName(name);
         student.setClassNum(classNum);
+        student.setIsAttend(true);
+        student.setSchool(school); // Schoolを設定
+
+		System.out.println(entYear);
+		System.out.println(no);
+		System.out.println(name);
+		System.out.println(classNum);
 
         // StudentDAOインスタンスを生成
         StudentDao dao = new StudentDao();
