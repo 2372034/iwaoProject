22014a59e52510687b8fe8ce25d095b69ed5f494
threi309 <ooi2372405@stu.o-hara.ac.jp>
2024-06-19 10:35:09 +0900

    学生変更のAction更新

--- src/score/StudentUpdateAction.java
+++ src/score/StudentUpdateAction.java
@@ -3,13 +3,19 @@ package score;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.Student;
+import dao.StudentDao;
 import tool.Action;
 
 public class StudentUpdateAction extends Action {
     public void execute(
         HttpServletRequest request, HttpServletResponse response
     ) throws Exception {
+    	String studentNo = request.getParameter("no");
+        StudentDao studentDao = new StudentDao();
+        Student student = studentDao.get(studentNo);
 
+        request.setAttribute("student", student);
 
         // JSPへのフォワード
         request.getRequestDispatcher("/score/student_change.jsp").forward(request, response);
